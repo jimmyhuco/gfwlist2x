11ce5a693712eb274ae9e759a6858561b310fc03
Jimmy Hu <jimmyhu.co@gmail.com>
2017-09-01 01:25:13 +0800

    add extraRules file check

--- app/Main.hs
+++ app/Main.hs
@@ -9,12 +9,12 @@ import GFW.Args
 import GFW.Converters.DNSMasq
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
+import System.Directory (doesFileExist)
 import System.Exit (exitFailure, exitSuccess)
 
 gfwlistTxtURL :: String
 gfwlistTxtURL =
   "https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt"
-
 
 downloadGFWList :: IO L.ByteString
 downloadGFWList = do
@@ -26,10 +26,16 @@ downloadGFWList = do
 process :: Args -> IO ()
 process (Args "dnsmasq" d i p) = do
   encoded <- downloadGFWList
-  usersRules <- readFile ".extraRules.gfw"
+  exists <- doesFileExist extraFilename
+  usersRules <-
+    if exists
+      then readFile extraFilename
+      else return ""
   generate p $ generateConf ((decode' encoded) ++ usersRules) d i
   where
     decode' bs = B8.unpack $ decodeLenient $ L.toStrict bs
+    extraFilename = ".extraRules.gfw"
+
 process (Args c _ _ _) =
   putStrLn ("Converter " ++ c ++ " is not supported!") >> exitFailure
 
